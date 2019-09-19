25f76220a407bb87d3b29e79d4e7c278795e1544
t00199548 <andypete12@gmail.com>
2019-09-19 12:18:52 +0100

    First exercise of Problem set 1

--- src/Problem1.java
+++ src/Problem1.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
 public class Problem1 {
+
+
     public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please enter the Exchange Rate: ");
+        float exchange =in.nextFloat();
+
+
+        System.out.print("Enter amount: ");
+
+        float amount = in.nextFloat();
+
+        while(amount>0)
+        {
+
+
+            float result = amount*exchange;
+
+            System.out.println("The Result is " + result);
+            System.out.print("Enter amount: ");
+            amount = in.nextFloat();
+
+        }
+
 
     }
 }
